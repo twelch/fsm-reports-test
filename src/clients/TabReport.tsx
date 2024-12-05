59e8453a0e6be878fe0441c8ae372d1c39aa2f41
Tim Welch <tim.j.welch@gmail.com>
2024-12-04 16:47:42 -0800

    first commit

---
+++ src/clients/TabReport.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  SegmentControl,
+  ReportPage,
+  SketchAttributesCard,
+} from "@seasketch/geoprocessing/client-ui";
+import Translator from "../components/TranslatorAsync.js";
+import { SimpleCard } from "../components/SimpleCard.js";
+
+const enableAllTabs = false;
+const BaseReport = () => {
+  const { t } = useTranslation();
+  const segments = [{ id: "OVERVIEW", label: t("Overview") }];
+  const [tab, setTab] = useState<string>("OVERVIEW");
+
+  return (
+    <>
+      <div style={{ marginTop: 5 }}>
+        <SegmentControl
+          value={tab}
+          onClick={(segment) => setTab(segment)}
+          segments={segments}
+        />
+      </div>
+      <ReportPage hidden={!enableAllTabs && tab !== "OVERVIEW"}>
+        <SimpleCard />
+        <SketchAttributesCard autoHide />
+      </ReportPage>
+    </>
+  );
+};
+
+// Named export loaded by storybook
+export const TabReport = () => {
+  return (
+    <Translator>
+      <BaseReport />
+    </Translator>
+  );
+};
+
+// Default export lazy-loaded by production ReportApp
+export default TabReport;
